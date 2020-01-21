b63fcce608a8658aab2decc0d8c36db2414779b7
Misha <mglsh@ukr.net>
2020-01-21 20:45:23 +0200

    15

---
+++ target/clever-server-v3/WEB-INF/classes/db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE 	zw0zfxcbeqwdox0e.device (
+
+id INT NOT NULL,
+
+name VARCHAR(255) NULL,
+
+email VARCHAR(255) NULL,
+
+phone VARCHAR(45) NULL,
+
+PRIMARY KEY (id));
+
+ALTER TABLE 	zw0zfxcbeqwdox0e.device
+
+CHANGE COLUMN id id INT(11) NOT NULL AUTO_INCREMENT ,
+
+ADD UNIQUE INDEX id_UNIQUE (id ASC);
